8a643f94b55ad2df535b0e968df4b8103898d6cc
Mofyally02 <mofyally02@gmail.com>
2023-06-13 00:48:27 +0300

    Java

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
-function factorial (n) {
-    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+
+function factorial (a) {
+    if (a === 1) {
+      return 1;
+    }
+    return factorial(a - 1) * a;
   }
   
-  console.log(factorial(Number(process.argv[2])));
+  const num = parseInt(process.argv[2]);
   
+  if (Number.isNaN(num)) {
+    console.log('1');
+  } else {
+    console.log(factorial(num));
+  }
