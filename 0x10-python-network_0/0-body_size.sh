156c8f577fee1e96206c6db4f5abde2b480c6a32
Mofyally02 <mofyally02@gmail.com>
2023-07-01 00:55:44 +0300

    Update Python

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes a URL, sends request to URL and displays size of the response body.
-curl -sI "$1" | grep -i "Content-Length" | cut -d : -f 2 | awk '{$1=$1};1'
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
