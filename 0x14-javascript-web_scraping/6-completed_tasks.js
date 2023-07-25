82de4bf8d9a09fe1e7d5d256110429280c0d20c3
Mofyally02 <mofyally02@gmail.com>
2023-07-25 23:39:29 +0300

    Update

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], (err, res, body) => {
-  if (err) console.log(err);
-  if (body) {
-    const todos = JSON.parse(body);
-
-    const done = {};
-    for (const todo of todos) {
-      if (todo.completed) {
-        done[todo.userId] ? done[todo.userId]++ : (done[todo.userId] = 1);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-
-    if (done) console.log(done);
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
